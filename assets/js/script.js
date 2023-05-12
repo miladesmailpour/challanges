0ec1aebd086a97e5306465c7a708d7e4f778a25f
miladesmailpour <milad.esmailpour@gmail.com>
2023-05-12 00:46:13 -0400

    save to localStorage functionality added to project

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,9 +5,15 @@ $(
     function () {
         var STORE_NAME = 'workDayScheduler'
         var currentDay = $('#currentDay')
+        var saveBtn = $('.saveBtn')
         var workDayScheduler = []
+
         load()
-        save(workDayScheduler)
+        saveBtn.on('click', saveHandler)
+
+        // workDayScheduler.push({ 'hour-10': 'test1' })
+        // // console.log(workDayScheduler)
+        // save(workDayScheduler)
 
 
 
@@ -17,24 +23,45 @@ $(
         // function? How can DOM traversal be used to get the "hour-x" id of the
         // time-block containing the button that was clicked? How might the id be
         // useful when saving the description in local storage?
+        //svae data to localStorage
         function save(data) {
-            var store = localStorage.getItem(STORE_NAME)
-            if (!store) {
-                store = JSON.stringify(data)
-                localStorage.setItem(STORE_NAME, store)
-            } else (
-                localStorage.setItem(STORE_NAME, store)
-            )
+            var store = JSON.stringify(data)
+            localStorage.setItem(STORE_NAME, store)
         }
+        // retrieve data from localStorage
         function load() {
             var store = localStorage.getItem(STORE_NAME)
             if (!store) {
+                var store = JSON.stringify(workDayScheduler)
                 console.log('store NOT founded!', 'store initiated as empty object just now!.')
-                localStorage.setItem(STORE_NAME, workDayScheduler)
-            } else (
-                workDayScheduler = store
-            )
+                localStorage.setItem(STORE_NAME, store)
+            } else {
+                workDayScheduler = JSON.parse(store)
+            }
         }
+        // handling save button click and save the related hour notes
+        function saveHandler() {
+            var parentEl = $(this).parent()
+            var timeKey = parentEl.attr('id')
+            var timeNote = parentEl.children('textarea').val()
+            var flag = true
+            load()
+            if (workDayScheduler.length > 0) {
+                for (var i = 0; i < workDayScheduler.length; i++) {
+                    if (Object.keys(workDayScheduler[i]) == timeKey) {
+                        var tmpHour = workDayScheduler[i]
+                        tmpHour = Object.assign(tmpHour, { [timeKey.trim()]: timeNote.trim() })
+                        workDayScheduler = Object.assign(workDayScheduler, { [timeKey.trim()]: timeNote.trim() })
+                        flag = false
+                    }
+                }
+            }
+            if (flag) {
+                workDayScheduler.push({ [timeKey.trim()]: timeNote.trim() })
+            }
+            save(workDayScheduler)
+        }
+
 
         // TODO: Add code to apply the past, present, or future class to each time
         // block by comparing the id to the current hour. HINTS: How can the id
