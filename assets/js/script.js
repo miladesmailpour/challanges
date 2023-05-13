871d52e8ab1370f1ce7cf797134c3aad2f0e39d7
miladesmailpour <milad.esmailpour@gmail.com>
2023-05-13 18:49:22 -0400

    html elements and requires id added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,11 @@
 console.log('I\'m in')
+
+var API_KEY = '46e7e576f7714418de08fb8d32cdfc56'
+
+var btn = document.getElementById('history-btn')
+var c = document.getElementById('search-history')
+
+btn.addEventListener('click', function () {
+    btn.setAttribute("style", 'display: none;')
+    c.setAttribute('style', 'display:block')
+})
