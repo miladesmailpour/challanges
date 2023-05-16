a949cf438d478cd20f92419bfff081f8c5cb5337
miladesmailpour <milad.esmailpour@gmail.com>
2023-05-16 03:52:16 -0400

    forecast added to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,23 +10,30 @@ var c = document.getElementById('search-history')
 var userInput = document.getElementById('user-input')
 
 var cities = []
-var forecast = {}
+var forecast = []
+var dayInfo = {
+    date: '',
+    temp: 0,
+    wind: 0,
+    humidity: 0,
+    img: '',
+    imgDesc: ''
+}
 
 load()
 
 btn.addEventListener('click', function () {
     // btn.setAttribute("style", 'display: none;')
     // c.setAttribute('style', 'display:block')
-    // cities.unshift(userInput.value)
+    save(userInput.value)
     REQUEST_URL = FORECAST_URL + userInput.value.trim() + API_KEY
-    console.log(REQUEST_URL)
     getData(REQUEST_URL)
-    console.log(save(userInput.value))
-
+    dataCollector()
+    // saveForecast('forecast')
 })
 
 
-
+// fetch the forcase data from open weather api
 function getData(endPoint) {
     fetch(endPoint)
         .then(function (response) {
@@ -37,11 +44,21 @@ function getData(endPoint) {
                 return response.json()
             }
         })
-        .then(function (data) {
-            console.log(data.list[0])
+        .then(function (dat) {
+            var data = dat.list
+            var day = 99
+            var tmp
+            console.log(data)
+            for (var i = 0; i < data.length; i++) {
+                tmp = (data[i].dt_txt).slice(8, 11)
+                if (day != tmp) {
+                    forecast.push(data[i])
+                    day = tmp
+                }
+            }
+            saveForecast('forecast')
         })
 }
-
 //svae data to localStorage
 function save(data) {
     var store
@@ -58,6 +75,10 @@ function save(data) {
     }
 
 }
+//svae forcast to localStorage
+function saveForecast(store) {
+    localStorage.setItem(store, JSON.stringify(forecast))
+}
 // retrieve data from localStorage
 function load() {
     var store = localStorage.getItem(STORE_NAME)
@@ -69,3 +90,34 @@ function load() {
         cities = JSON.parse(store)
     }
 }
+// collecting the required data for elements
+function dataCollector() {
+    console.log(data)
+    for (var i = 0; i < data.length; i++) {
+        tmp = (data[i].dt_txt).slice(8, 11)
+        if (day != tmp) {
+            console.log(tmp, data[i])
+            // dayInfo.date = (data[i].dt_txt).slice(0, 11)
+            // dayInfo.temp = data[i].main.temp
+            // dayInfo.wind = data[i].wind.speed
+            // dayInfo.humidity = data[i].main.humidity
+            // dayInfo.imgDesc = data[i].weather[0].description
+            // dayInfo.img = data[i].weather[0].icon
+            forecast.push(data[i])
+            day = tmp
+        }
+    }
+    saveForecast('forecast')
+    console.log(forecast)
+    setIcon()
+
+}
+// set the Icon for the forecast
+function setIcon() {
+    forecast = JSON.parse(localStorage.getItem('forecast'))
+    for (var i = 0; i < forecast.length; i++) {
+        console.log('tmp', forecast[i])
+
+    }
+}
+
