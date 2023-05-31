c8530291e825dd4e8ce8f2fd6dcae1c50d320e99
miladesmailpour <milad.esmailpour@gmail.com>
2023-05-30 21:50:22 -0400

    Circle class added and inherent from Shape

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
+const Circle = require('./lib/Circle.js')
 const Shape = require('./lib/Shape.js')
 
 const init = () => {
-    const shape = new Shape()
-    console.log(shape)
+    const shape = new Shape({
+        hight: 13,
+        width: 13,
+        fillColor: 'red',
+        strokeColor: 'red',
+        strokeWidth: 13
+    })
+    const circle = new Circle({
+        hight: 13,
+        width: 13,
+        fillColor: 'red',
+        strokeColor: 'red',
+        strokeWidth: 13,
+        redius: 113
+    })
+    console.log(shape, circle)
 }
 init()
