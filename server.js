6b68b56de7f1b1b836f507b93e60820d67716f9b
miladesmailpour <milad.esmailpour@gmail.com>
2023-06-02 03:18:56 -0400

    route added to redirect * to index.html

--- server.js
+++ server.js
@@ -11,4 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 // app.use("/api", api);
 app.use(express.static("public"));
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
 app.listen(PORT, () => console.log(`App serve at http://localhost:${PORT}`));
