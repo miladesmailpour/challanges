dd76b257f8ef1483fca8c1ff9ddfcd470519780d
miladesmailpour <milad.esmailpour@gmail.com>
2023-06-02 03:13:25 -0400

    middleware added to the server.js

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
-const express = require('express')
-const path = require('path')
+const express = require("express");
+const path = require("path");
+const api = require("./routes/index.js");
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 
-const app = express()
+const app = express();
+// Middleware for parsing JSON, urlencoded form data, APIs and etc.
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use("/api", api);
+app.use(express.static("public"));
 
-
-
-
-
-
-app.listen(PORT, () =>
-    console.log(`App serve at http://localhost:${PORT}`)
-)
+app.listen(PORT, () => console.log(`App serve at http://localhost:${PORT}`));
