a238604e04e7d2e7203389bc64818a93f1809ad3
miladesmailpour <milad.esmailpour@gmail.com>
2023-06-02 02:38:55 -0400

    server is up!

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const path = require('path')
+
+const PORT = process.env.PORT || 3001
+
+const app = express()
+
+
+
+
+
+
+app.listen(PORT, () =>
+    console.log(`App serve at http://localhost:${PORT}`)
+)
