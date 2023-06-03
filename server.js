423ccc70f606cc99cba6c13a703ed9013b7c836f
miladesmailpour <milad.esmailpour@gmail.com>
2023-06-02 22:02:41 -0400

    delete button handling the not delete from list

--- server.js
+++ server.js
@@ -13,11 +13,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api", api);
 app.use(express.static("public"));
 
+app.get("/notes", (req, res) => {
+  console.log("----------------");
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
-app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/notes.html"));
-});
 app.listen(PORT, () => console.log(`App serve at http://localhost:${PORT}`));
