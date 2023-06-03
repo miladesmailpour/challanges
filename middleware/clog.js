65ae7b97ae02d70a47c0474171be417a6bb585cb
miladesmailpour <milad.esmailpour@gmail.com>
2023-06-02 21:40:50 -0400

    notes get route added to redirect to notes.html

--- middleware/clog.js
+++ middleware/clog.js
@@ -1,17 +1,21 @@
 // Custom middleware that logs out the type and path of each request to the server
 const clog = (req, res, next) => {
-  const fgCyan = '\x1b[36m';
+  const fgCyan = "\x1b[36m";
   switch (req.method) {
-    case 'GET': {
+    case "GET": {
       console.info(`📗 ${fgCyan}${req.method} request to ${req.path}`);
       break;
     }
-    case 'POST': {
+    case "POST": {
       console.info(`📘 ${fgCyan}${req.method} request to ${req.path}`);
       break;
     }
+    case "DELETE": {
+      console.info(`📕 ${fgCyan}${req.method} request to ${req.path}`);
+      break;
+    }
     default:
-      console.log(`📙${fgCyan}${req.method} request to ${req.path}`);
+      console.log(`📙 ${fgCyan}${req.method} request to ${req.path}`);
   }
 
   next();
