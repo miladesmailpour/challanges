9367d408bb47954c8666c37174f30ee0075d8da1
miladesmailpour <milad.esmailpour@gmail.com>
2023-06-02 21:22:14 -0400

    notes get route added to return a specific note

--- routes/notes.js
+++ routes/notes.js
@@ -11,22 +11,23 @@ notes.get("/", async (req, res) => {
 });
 
 // GET Route for a specific note
-// tips.get('/:tip_id', (req, res) => {
-//   const tipId = req.params.tip_id;
-//   readFromFile('./db/db.json')
-//     .then((data) => JSON.parse(data))
-//     .then((json) => {
-//       const result = json.filter((note) => note.tip_id === tipId);
-//       return result.length > 0
-//         ? res.json(result)
-//         : res.json('No tip with that ID');
-//     });
-// });
+notes.get("/:note_id", (req, res) => {
+  console.info(`${req.method} request received for notes router`);
+  const noteId = req.params.note_id;
+  readFromFile("./db/db.json")
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      const result = json.filter((note) => note.note_id === noteId);
+      return result.length > 0
+        ? res.json(result)
+        : res.json("No note with that ID");
+    });
+});
 // POST Route for submitting note
 notes.post("/", (req, res) => {
+  console.info(`${req.method} request received for notes router`);
   console.log(req.body);
-
-  const { title, text} = req.body;
+  const { title, text } = req.body;
 
   if (req.body) {
     const newNote = {
