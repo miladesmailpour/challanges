ce56ad07169ca68da16177a603ec03e9de5f9998
miladesmailpour <milad.esmailpour@gmail.com>
2023-06-02 21:13:33 -0400

    notes post route added to save a note

--- routes/notes.js
+++ routes/notes.js
@@ -9,5 +9,36 @@ notes.get("/", async (req, res) => {
   console.info(`${req.method} request received for notes router`);
   readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
 });
-// POST Route for submitting notes
+
+// GET Route for a specific note
+// tips.get('/:tip_id', (req, res) => {
+//   const tipId = req.params.tip_id;
+//   readFromFile('./db/db.json')
+//     .then((data) => JSON.parse(data))
+//     .then((json) => {
+//       const result = json.filter((note) => note.tip_id === tipId);
+//       return result.length > 0
+//         ? res.json(result)
+//         : res.json('No tip with that ID');
+//     });
+// });
+// POST Route for submitting note
+notes.post("/", (req, res) => {
+  console.log(req.body);
+
+  const { title, text} = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      note_id: uuid(),
+    };
+
+    readAndAppend(newNote, "./db/db.json");
+    res.json(`note added successfully 🚀`);
+  } else {
+    res.error("Error in adding note");
+  }
+});
 module.exports = notes;
--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const path = require("path");
 const api = require("./routes/index");
+const { clog } = require("./middleware/clog");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 // Middleware for parsing JSON, urlencoded form data, APIs and etc.
+app.use(clog);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api", api);
