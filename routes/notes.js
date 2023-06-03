c8ffa93f9177ea9485b05943d7a3ea18b76dcd8c
miladesmailpour <milad.esmailpour@gmail.com>
2023-06-02 21:28:00 -0400

    notes delete route added to remove a specific note

--- routes/notes.js
+++ routes/notes.js
@@ -1,6 +1,10 @@
 const notes = require("express").Router();
 // reading and writing to the JSON file
-const { readFromFile, readAndAppend } = require("../helper/fsUtils");
+const {
+  readFromFile,
+  readAndAppend,
+  writeToFile,
+} = require("../helper/fsUtils");
 // generate unique id
 const uuid = require("../helper/uuid");
 
@@ -41,5 +45,20 @@ notes.post("/", (req, res) => {
   } else {
     res.error("Error in adding note");
   }
+});
+// DELETE Route for a specific note
+notes.delete("/:note_id", (req, res) => {
+  const noteId = req.params.note_id;
+  readFromFile("./db/db.json")
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      const result = json.filter((note) => note.note_id !== noteId);
+
+      // Save that array to db (json file)
+      writeToFile("./db/db.json", result);
+
+      // Respond to the DELETE request
+      res.json(`Item ${noteId} has been deleted 🗑️`);
+    });
 });
 module.exports = notes;
