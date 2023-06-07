5d1244a1ab50255373f694b230f13c3bdb5f971b
miladesmailpour <milad.esmailpour@gmail.com>
2023-06-06 22:01:45 -0400

    relationship between tables added

--- db/schema.sql
+++ db/schema.sql
@@ -5,18 +5,25 @@ USE employee_tracker_db;
 
 CREATE TABLE department(
     id INT NOT NULL,
-    name varchar(30) NOT NULL
+    name varchar(30) NOT ,
+    PRIMARY KEY (id)
 );
 CREATE TABLE role(
     id INT NOT NULL,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL,
-    department_id INT
+    department_id INT,
+    PRIMARY KEY (id)
+    FOREIGN KEY (department_id) REFERENCES department(id)
+    ON DELETE SET NULL
 );
 CREATE TABLE employee(
     id INT NOT NULL,
     first_name VARCHAR(30) NOT NULL,
     last_name varchar(30) NOT NULL,
     role_id INT,
-    manager_id INT
+    manager_id INT,
+    PRIMARY KEY (id)
+    FOREIGN KEY (role_id) REFERENCES role(id)
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
