b58d8a1b409c99ab2ac37c2302aae091f922ccec
miladesmailpour <milad.esmailpour@gmail.com>
2023-06-06 22:16:58 -0400

    adding the server.js and express to the project

--- db/schema.sql
+++ db/schema.sql
@@ -4,26 +4,27 @@ CREATE DATABASE employee_tracker_db;
 USE employee_tracker_db;
 
 CREATE TABLE department(
-    id INT NOT NULL,
-    name varchar(30) NOT ,
+    id INT NOT NULL AUTO_INCREMENT,
+    name varchar(30) NOT NULL,
     PRIMARY KEY (id)
 );
 CREATE TABLE role(
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL,
     department_id INT,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
     FOREIGN KEY (department_id) REFERENCES department(id)
     ON DELETE SET NULL
 );
 CREATE TABLE employee(
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
-    last_name varchar(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
     role_id INT,
     manager_id INT,
-    PRIMARY KEY (id)
-    FOREIGN KEY (role_id) REFERENCES role(id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES role(id),
     FOREIGN KEY (manager_id) REFERENCES employee(id)
+    ON DELETE SET NULL
 );
