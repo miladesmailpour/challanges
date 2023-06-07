543c60b932f5ff3d6bd1c22db9b5c51dde0173ce
miladesmailpour <milad.esmailpour@gmail.com>
2023-06-07 00:55:44 -0400

    add put route to update the employee role and fix the bug in get route

--- routes/employee.js
+++ routes/employee.js
@@ -1,7 +1,7 @@
 const employee = require("express").Router();
 const db = require("../config/connection.js");
 
-// Create a employee
+// Create an employee
 employee.post("/", ({ body }, res) => {
   const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id)
     VALUES (?, ?, ?, ?)`;
@@ -26,11 +26,10 @@ employee.post("/", ({ body }, res) => {
 
 // Read all employee
 employee.get("/", (req, res) => {
-  const sql = `SELECT e.first_name, e.last_name, em.last_name, r.title, r.salary, d.name 
-  from employee e 
-  join employee em on e.id = em.manager_id
-  join role r on e.role_id = r.id
-  join department d on d.id = r.department_id`;
+  const sql = `select e.id, e.first_name, e.last_name, em.last_name, r.title, r.salary, d.name from employee e 
+  left join employee em on e.id=em.manager_id 
+  left join role r on e.role_id=r.id 
+  left join department d on r.department_id=d.id;`;
 
   db.query(sql, (err, rows) => {
     if (err) {
@@ -44,7 +43,7 @@ employee.get("/", (req, res) => {
   });
 });
 
-// Delete a employee
+// Delete an employee
 employee.delete("/:id", (req, res) => {
   const sql = `DELETE FROM employee WHERE id = ?`;
   const params = [req.params.id];
@@ -54,7 +53,7 @@ employee.delete("/:id", (req, res) => {
       res.statusMessage(400).json({ error: res.message });
     } else if (!result.affectedRows) {
       res.json({
-        message: "department not found",
+        message: "employee not found",
       });
     } else {
       res.json({
@@ -65,5 +64,26 @@ employee.delete("/:id", (req, res) => {
     }
   });
 });
+// Update an employee role
+employee.put("/:id", (req, res) => {
+  const sql = `UPDATE employee SET role_id = ? WHERE id = ?`;
+  const params = [req.body.role_id, req.params.id];
 
-module.exports = role;
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+    } else if (!result.affectedRows) {
+      res.json({
+        message: "employee not found",
+      });
+    } else {
+      res.json({
+        message: "success",
+        data: req.body,
+        changes: result.affectedRows,
+      });
+    }
+  });
+});
+
+module.exports = employee;
--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,12 @@ const express = require("express");
 // Import our modular routers for
 const departmentRouter = require("./department");
 const roleRouter = require("./role");
+const employeeRouter = require("./employee");
 
 const app = express();
 
 app.use("/department", departmentRouter);
 app.use("/role", roleRouter);
+app.use("/employee", employeeRouter);
 
 module.exports = app;
