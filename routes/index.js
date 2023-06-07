2b4931921a908cb84d1d70bb90b6d1de2f2a1bae
miladesmailpour <milad.esmailpour@gmail.com>
2023-06-06 23:28:08 -0400

    require routes for role added:post,get,delete

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,11 @@ const express = require("express");
 
 // Import our modular routers for
 const departmentRouter = require("./department");
+const roleRouter = require("./role");
 
 const app = express();
 
 app.use("/department", departmentRouter);
+app.use("/role", roleRouter);
 
 module.exports = app;
