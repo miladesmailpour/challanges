a74d1dfb117755ba5e5210e8e31997d22908f35e
miladesmailpour <milad.esmailpour@gmail.com>
2023-06-06 21:53:36 -0400

    tables added to the schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,22 @@
 DROP DATABASE IF EXISTS employee_tracker_db;
 CREATE DATABASE employee_tracker_db;
+
+USE employee_tracker_db;
+
+CREATE TABLE department(
+    id INT NOT NULL,
+    name varchar(30) NOT NULL
+);
+CREATE TABLE role(
+    id INT NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL,
+    department_id INT
+);
+CREATE TABLE employee(
+    id INT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name varchar(30) NOT NULL,
+    role_id INT,
+    manager_id INT
+);
